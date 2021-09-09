c972a5aec42ca0489d492901e026da90c54e0d3f
Renan Gustavo <renandevfullstack@gmail.com>
2021-09-09 19:01:02 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,575 @@
+const dados = [
+    {
+        id: 1,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 2,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 3,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 4,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 5,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 6,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 7,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 8,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 9,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 10,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 11,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 12,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 13,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 14,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 15,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 16,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 17,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 18,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 19,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 20,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 21,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },{
+        id: 22,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 23,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 24,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 25,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 26,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },{
+        id: 27,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 28,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 29,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 30,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },{
+        id: 31,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 32,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 33,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 34,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 35,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 36,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 37,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },{
+        id: 38,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 39,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 40,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 41,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 42,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 43,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 44,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 45,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 46,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 47,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 48,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 49,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 50,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 51,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 52,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 53,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 54,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 55,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 56,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 57,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 58,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+    {
+        id: 59,
+        transacation: '02-846015225',
+        comissao: '0,13',
+        status: 'Pendente',
+        dataDaCompra: '14-07-2021',
+        DatadaAtualizacao: '16-07-2021'
+    },
+]
+
+function pagination (page, total, limit) {
+    var pageSize = Math.ceil(total / limit);
+
+    var _pagination = {
+        page: page,
+        total: total,
+        limit: limit,
+        pages: pageSize,
+    };
+
+    if(page > 1){
+      var prev = page - 1;
+      _pagination.previous = prev;
+    }
+
+    var remaining = total - (page * limit);
+
+    if(remaining > 0){
+      _pagination.next = page + 1;
+    }
+
+    return _pagination;
+}
+function calcPagination(pageQuery){
+    const page = parseInt(pageQuery) || 1
+    const limit = 10
+    const offset = (page - 1) * limit
+    const total = dados.length;
+    const items = dados.slice(offset, offset + limit);
+
+    document.querySelector(".records").innerHTML = `
+        <div>Mostrando de ${offset + 1} até ${offset + limit} de ${total} registros</div>`
+
+    return {
+        page,
+        limit,
+        offset,
+        total,
+        items
+    };
+};
+
+const paginatioCalc01 = calcPagination(1);
+const paginationResult = pagination(paginatioCalc01.page, paginatioCalc01.total, paginatioCalc01.limit);
+
+paginatioCalc01.items.map(item => {
+    document.querySelector("table tbody").innerHTML += `
+        <tr>
+            <td>${item.id}</td>
+            <td>${item.transacation}</td>
+            <td>${item.comissao}</td>
+            <td>${item.status}</td>
+            <td>${item.dataDaCompra}</td>
+            <td>${item.DatadaAtualizacao}</td>
+        </tr>
+    `
+});
+
+const paginationWrapper = document.querySelector("ul.pagination");
+
+for(let i = 0; i < paginationResult.pages; i++){ // adiciona os botoes
+    const active = paginationResult.page === i + 1 ? 'active' : '' // se estiver na pagina atual, se atribui "active".
+
+    paginationWrapper.innerHTML += `
+    <li class="page-item ${active}" onClick="upDateThePagination(${i})"">
+        <a class="page-link">${i + 1}</a>
+    </li>`;
+};
+
+function renderButtons (){
+    paginationWrapper.innerHTML = "";
+    for(let i = 0; i < paginationResult.pages; i++){ // adiciona os botoes
+        const active = paginationResult.page === i + 1 ? 'active' : '' // se estiver na pagina atual, se atribui "active".
+
+        paginationWrapper.innerHTML += `
+        <li class="page-item ${active}" onClick="upDateThePagination(${i})"">
+            <a class="page-link">${i + 1}</a>
+        </li>`;
+    };
+};
+
+function upDateThePagination(paginationNum) {
+    paginationNum++
+    paginationResult.page = paginationNum;
+    pageQuery = paginationNum;
+
+    renderButtons();
+
+    const paginatioCalc02 = calcPagination(paginationNum);
+    document.querySelector("table tbody").innerHTML = "";
+
+    paginatioCalc02.items.map(item => {
+        document.querySelector("table tbody").innerHTML += `
+            <tr>
+                <td>${item.id}</td>
+                <td>${item.transacation}</td>
+                <td>${item.comissao}</td>
+                <td>${item.status}</td>
+                <td>${item.dataDaCompra}</td>
+                <td>${item.DatadaAtualizacao}</td>
+            </tr>
+        `
+    });
+}
